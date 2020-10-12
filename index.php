718492610d8fb5258bcc812c9635bc6418bbb053
BarbaraGonthier <69505965+BarbaraGonthier@users.noreply.github.com>
2020-10-12 09:17:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,46 +8,45 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
+<?php
 
     require_once '_connec.php';
 
     $pdo = new \PDO(DSN, USER, PASS);
-    
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $firstname = trim($_POST['firstname']);
-    $lastname = trim($_POST['lastname']);
+        $firstname = trim($_POST['firstname']);
+        $lastname = trim($_POST['lastname']);
+
+
+        $query = 'INSERT INTO friend (firstname, lastname) VALUES (:firstname, :lastname)';
+        $statement = $pdo->prepare($query);
+
+        $statement->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
+        $statement->bindValue(':lastname', $lastname, \PDO::PARAM_STR);
+
+
+        $statement->execute();
     }
-    
-   
-    $query = 'INSERT INTO friend (firstname, lastname) VALUES (:firstname, :lastname)';
-    $statement = $pdo->prepare($query);
-    
-    $statement->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
-    $statement->bindValue(':lastname', $lastname, \PDO::PARAM_STR);
 
-
-    $statement->execute();
-    
     $query = "SELECT * FROM friend";
     $statement = $pdo->query($query);
     $friends = $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
     ?>
-    
-    <section id="friends-list">
-        <?php foreach($friends as $friend) { ?>
-        <div> <?php echo $friend['firstname'] . ' ' . $friend['lastname']; ?> </div>
-        <?php } ?>
-    </section>
 
-    <form method="post" action="">
-        <label for="firstname">Firstame :</label>
-        <input type="text" id="firstname" name="firstname">
-        <label for="lastname">Lastname :</label>
-        <input type="text" id="lastname" name="lastname">
-        <button type="submit">submit</button>
-    </form>
+<section id="friends-list">
+    <?php foreach($friends as $friend) { ?>
+    <div> <?php echo $friend['firstname'] . ' ' . $friend['lastname']; ?> </div>
+    <?php } ?>
+</section>
+
+<form method="post" action="">
+    <label for="firstname">Firstame :</label>
+    <input type="text" id="firstname" name="firstname">
+    <label for="lastname">Lastname :</label>
+    <input type="text" id="lastname" name="lastname">
+    <button type="submit">submit</button>
+</form>
 
 </body>
 </html>
